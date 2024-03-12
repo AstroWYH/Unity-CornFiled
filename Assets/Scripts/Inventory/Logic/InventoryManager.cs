a29f74928b5b83e4de28923b435aec5871bc3cc8
astrowang <819369336@qq.com>
2024-03-12 23:18:44 +0800

    312

--- Assets/Scripts/Inventory/Logic/InventoryManager.cs
+++ Assets/Scripts/Inventory/Logic/InventoryManager.cs
@@ -4,7 +4,10 @@ namespace MFarm.Inventory
 {
     public class InventoryManager : Singleton<InventoryManager>
     {
+        [Header("物品数据")]
         public ItemDataList_SO itemDataList_SO;
+        [Header("背包数据")]
+        public InventoryBag_SO playerBag;
 
         /// <summary>
         /// 通过ID返回物品信息
@@ -14,6 +17,20 @@ namespace MFarm.Inventory
         public ItemDetails GetItemDetails(int ID)
         {
             return itemDataList_SO.itemDetailsList.Find(i => i.itemID == ID);
+        }
+
+        /// <summary>
+        /// 添加物品到Player背包里
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="toDestory">是否要销毁物品</param>
+        public void AddItem(Item item, bool toDestory)
+        {
+            Debug.Log(GetItemDetails(item.itemID).itemID + " Name: " + GetItemDetails(item.itemID).itemName);
+            if (toDestory)
+            {
+                Destroy(item.gameObject);
+            }
         }
     }
 }
