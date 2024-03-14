43a49991253fb985b520590d65eab8e64c012117
819369336@qq.com <819369336@qq.com>
2024-03-14 21:53:14 +0800

    25 start

--- Assets/Scripts/Inventory/UI/SlotUI.cs
+++ Assets/Scripts/Inventory/UI/SlotUI.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SlotUI : MonoBehaviour
+namespace MFarm.Inventory
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SlotUI : MonoBehaviour
     {
-        
-    }
+        [Header("组件获取")]
+        [SerializeField] private Image slotImage;
+        [SerializeField] private TextMeshProUGUI amountText;
+        public Image slotHightlight;
+        [SerializeField] private Button button;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("格子类型")]
+        public SlotType slotType;
+        public bool isSelected;
+
+        //物品信息
+        public ItemDetails itemDetails;
+        public int itemAmount;
+
+        private void Start()
+        {
+            isSelected = false;
+            if (itemDetails.itemID == 0)
+            {
+                UpdateEmptySlot();
+            }
+        }
+
+        /// <summary>
+        /// 更新格子UI和信息
+        /// </summary>
+        /// <param name="item">ItemDetails</param>
+        /// <param name="amount">持有数量</param>
+        public void UpdateSlot(ItemDetails item, int amount)
+        {
+            itemDetails = item;
+            slotImage.sprite = item.itemIcon;
+            itemAmount = amount;
+            amountText.text = amount.ToString();
+            slotImage.enabled = true;
+            button.interactable = true;
+        }
+
+        /// <summary>
+        /// 将Slot更新为空
+        /// </summary>
+        public void UpdateEmptySlot()
+        {
+            if (isSelected)
+            {
+                isSelected = false;
+            }
+            itemDetails = null;
+            slotImage.enabled = false;
+            amountText.text = string.Empty;
+            button.interactable = false;
+        }
     }
 }
