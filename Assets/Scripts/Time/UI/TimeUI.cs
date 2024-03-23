b4e9c4131afef7f121893b1d24e8afb667ed1128
astrowang <819369336@qq.com>
2024-03-23 18:00:33 +0800

    37 5.16

---
+++ Assets/Scripts/Time/UI/TimeUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeUI : MonoBehaviour
+{
+    public RectTransform dayNightImage;
+    public RectTransform clockParent;
+    public Image seasonImage;
+    public TextMeshProUGUI dateText;
+    public TextMeshProUGUI timeText;
+    public Sprite[] seasonSprites;
+
+    private List<GameObject> clockBlocks = new List<GameObject>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < clockParent.childCount; i++)
+        {
+            clockBlocks.Add(clockParent.GetChild(i).gameObject);
+            clockParent.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Utilities/EventHandler.cs
+++ Assets/Scripts/Utilities/EventHandler.cs
@@ -21,4 +21,16 @@ public static class EventHandler
     {
         ItemSelectedEvent?.Invoke(itemDetails, isSelected);
     }
+
+    public static event Action<int, int, int, Season> GameMinuteEvent;
+    public static void CallGameMinuteEvent(int minute, int hour, int day, Season season)
+    {
+        GameMinuteEvent?.Invoke(minute, hour, day, season);
+    }
+
+    public static event Action<int, Season> GameDayEvent;
+    public static void CallGameDayEvent(int day, Season season)
+    {
+        GameDayEvent?.Invoke(day, season);
+    }
 }
