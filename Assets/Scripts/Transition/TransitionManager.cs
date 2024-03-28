6f7904e05697780a3b74510f7f6a5872b54b9733
astrowang <819369336@qq.com>
2024-03-28 20:54:40 +0800

    41 14.15

--- Assets/Scripts/Transition/TransitionManager.cs
+++ Assets/Scripts/Transition/TransitionManager.cs
@@ -8,6 +8,7 @@ namespace MFarm.Transition
 {
     public class TransitionManager : MonoBehaviour
     {
+        [SceneName]
         public string startSceneName = string.Empty;
 
         private void OnEnable()
